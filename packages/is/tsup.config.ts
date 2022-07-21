58fd0a30689b45f8720b5f89c68efa14d4e50e5c
ww <1341265546@qq.com>
2022-07-21 10:01:41 +0800

    chore: use npm monorepo manage project

--- packages/is/tsup.config.ts
+++ packages/is/tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: ['./index.ts'],
   outDir: 'dist',
   dts: true,
   format: ['esm', 'cjs'],
   clean: true,
-  watch: true,
 })
+
