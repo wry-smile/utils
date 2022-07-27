5241e0e3ad373e0c3c48781f1be09eae39755068
ww <1341265546@qq.com>
2022-07-27 15:47:48 +0800

    chore: directory adjust

--- packages/dom-action/index.ts
+++ packages/dom-action/index.ts
@@ -1,3 +1,20 @@
+/**
+ * @description get node offset
+ * @param {HTMLElement} ele node
+ * @returns
+ */
+export function getOffset(ele: HTMLElement): { top: number; left: number } {
+  let el = ele
+  let _x = 0
+  let _y = 0
+  while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+    _x += el.offsetLeft - el.scrollLeft
+    _y += el.offsetTop - el.scrollTop
+    el = el.offsetParent as HTMLElement
+  }
+  return { top: _y, left: _x }
+}
+
 export function useDrag(dragEl: HTMLElement) {
   function getPxValue(value: string) {
     if (value.includes('px')) return Number(value.replace(/px/g, ''))
@@ -51,3 +68,4 @@ export function useDrag(dragEl: HTMLElement) {
     }
   })
 }
+
